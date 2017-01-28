0165b7b907ca465552d611c8cafde2d14b6c7bbb
emngaiden <emngaiden@hotmail.com>
2017-01-27 23:04:13 -0600

    Agregado form para subir noticias
    Fuck netbeans

---
+++ backend/models/NewsForm.php
@@ -0,0 +1,34 @@
+<?php
+namespace backend\models;
+
+use Yii;
+use yii\base\Model;
+use common\models\Noticia;
+
+class NewsForm extends Model
+{
+    public $imagen;
+    public $titulo;
+    public $descripcion;
+    public $link;
+    
+    public function rules()
+    {
+        return [
+            [['titulo','descripcion','link'],'required'],
+        ];
+    }
+    
+    public function addNew(){
+        if($this->validate()){
+            $noticia=new Noticia();
+            $noticia->titulo=$this->titulo;
+            $noticia->descripcion=$this->descripcion;
+            $noticia->link=$this->link;
+            $noticia->imagen=$this->imagen;
+            $noticia->user_id=Yii::$app->getUser()->getId();
+            $noticia->visitas=0;
+            return $noticia->save();
+        }
+    }
+}
